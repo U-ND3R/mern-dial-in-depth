22df91b2e2e06b85299565967c9068ba35707748
Artemij <artiomchrisonopulo@gmail.com>
2024-01-10 22:53:48 +0200

    Create Sign-up API route

--- api/index.js
+++ api/index.js
@@ -2,6 +2,7 @@ import express from 'express'
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js';
 
 dotenv.config();
 
@@ -14,8 +15,10 @@ mongoose
         console.log(err)
     });
 const app = express();
+app.use(express.json());
 app.listen(5000, () => {
     console.log('Server is running on port 5000');
 })
 
 app.use('/api/user', userRouter);
+app.use('/api/auth', authRouter);
